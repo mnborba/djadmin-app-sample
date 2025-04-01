4b38b3cd6e9407123eb8f2fa1267bf59ce98b417
Marcelo Borba <mnborba@gmail.com>
2025-04-01 15:48:30 -0300

    Atualização do Laytout Jazzmin

--- core/settings.py
+++ core/settings.py
@@ -127,35 +127,34 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-JAZZMIN_SETTINGS = {
-    # title of the window (Will default to current_admin_site.site_title if absent or None)
-    'site_title': 'SGP',
-
-    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    'site_header': 'SGP',
-
-    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    'site_brand': 'SGP',
-
-    'icons': {
-        'auth': 'fas fa-users-cog',
-        'auth.user': 'fas fa-user',
-        'auth.Group': 'fas fa-users',
-        'produtos.Marca': 'fas fa-copyright',
-        'produtos.Categoria': 'fas fa-object-group',
-        'produtos.Produto': 'fas fa-box',
-    },
-
-    # Welcome text on the login screen
-    'welcome_sign': 'Bem-vindo(a) ao SGP',
-
-    # Copyright on the footer
-    'copyright': 'PycodeBR LTDA',
-
-    # List of model admins to search from the search bar, search bar omitted if excluded
-    # If you want to use a single search field you dont need to use a list, you can use a simple string 
-    'search_model': ['products.Product',],
-
-    # Whether to show the UI customizer on the sidebar
-    'show_ui_builder': True,
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-indigo",
+    "navbar": "navbar-indigo navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "cerulean",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-outline-primary",
+        "secondary": "btn-outline-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
 }
