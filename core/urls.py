b33aece0073fdf1a6ef598ad160703a98b62ab23
Marcelo Borba <mnborba@gmail.com>
2025-04-01 00:30:21 -0300

    App concluído com ulrs apontndo para /

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     #path('grappelli/', include('grappelli.urls')),
-    path('admin/', admin.site.urls),    
+    path('', admin.site.urls),    
 ]
